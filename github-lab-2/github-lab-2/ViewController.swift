98851f13568af3280e8eb15897ebdeda20f16f21
Loris Mazloum <loris.mazloum@gmail.com>
2016-08-17 16:08:13 -0700

    creating xcode project

---
+++ github-lab-2/github-lab-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github-lab-2
+//
+//  Created by Loris Mazloum on 8/17/16.
+//  Copyright © 2016 Loris Mazloum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
